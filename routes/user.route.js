d4b94f572d8726ff12f0b2b659024e3f4f19edfb
Tri Vo <vocaotri445@gmail.com>
2021-05-19 13:17:55 +0700

    update login resgister

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var userController = require('../controllers/user.controller');
 const { checkSchema } = require('express-validator');
-var registrationSchema = require('../validates/user.validate')
+var { registrationSchema, loginSchema } = require('../validates/user.validate')
 var router = express.Router();
 router.get('/list-user', userController.listUsers);
 router.get('/user', userController.user)
 router.post('/add-user', checkSchema(registrationSchema), userController.addUser);
+router.post('/login', checkSchema(loginSchema), userController.login);
 router.put('/update-user', userController.updateUser);
 router.delete('/delete-user', userController.deleteUser);
 module.exports = router;
